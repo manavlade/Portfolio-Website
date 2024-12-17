e7cb33a1269106005fd2ab023a13fc2608210faf
manavlade <manavlade14690@gmail.com>
2024-12-18 00:28:15 +0530

    Added skills icon and socials

---
+++ Portfolio Website/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Navbar } from './Components/Shared/Navbar'
+import { IconCloudDemo } from './Components/Skills/SkillImage'
+
+function App() {
+  return (
+    <>
+      <div>
+       hiii
+       {/* <IconCloudDemo/> */}
+       {/* <Navbar/> */}
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ Portfolio Website/src/Components/Skills/SkillImage.jsx
@@ -0,0 +1,33 @@
+import IconCloud from "../ui/icon-cloud";
+
+
+const slugs = [
+    "typescript",
+    "javascript",
+    "python",
+    "java",
+    "react",
+    "html5",
+    "css3",
+    "nodedotjs",
+    "express",
+    "nextdotjs",
+    "mongodb",
+    "prisma",
+    "amazonaws",
+    "firebase",
+    "docker",
+    "git",
+    "github",
+    "gitlab",
+    "visualstudiocode",
+    "figma",
+];
+
+export function IconCloudDemo() {
+    return (
+        <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8 ">
+            <IconCloud iconSlugs={slugs} />
+        </div>
+    );
+}
